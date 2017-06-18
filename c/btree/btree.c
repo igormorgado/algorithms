bef0a09fd0697dd986f990ebeb357d1a1053ac51
igormorgado <igormorgado@users.noreply.github.com>
2017-06-18 16:53:00 -0300

    Fixed a issue in a logical operator

--- c/btree/btree.c
+++ c/btree/btree.c
@@ -10,9 +10,8 @@
 
 void newTree(struct Tree **T) 
 {
-  (*T)       = malloc(sizeof *(*T));
-
-  (*T)->root = NULL;
+  (*T)         = malloc(sizeof *(*T));
+  (*T)->root   = NULL;
   (*T)->height = 0;
 }
 
@@ -20,7 +19,6 @@ void newTree(struct Tree **T)
 void destroyTree(struct Tree **T)
 {
   destroyNodeByRef(&((*T)->root));
-  // (*T)->root = NULL;
   free(*T);
   *T = NULL;
 }
@@ -28,7 +26,7 @@ void destroyTree(struct Tree **T)
 
 void walkTreePre(struct Node *root) 
 {
-  if (root != NULL) {
+  if (root) {
     printNode(root);
     walkTreePre(root->left);
     walkTreePre(root->right);
@@ -38,7 +36,7 @@ void walkTreePre(struct Node *root)
 
 void walkTreePos(struct Node *root) 
 {
-  if (root != NULL) {
+  if (root) {
     walkTreePos(root->left);
     walkTreePos(root->right);
     printNode(root);
@@ -48,7 +46,7 @@ void walkTreePos(struct Node *root)
 
 void walkTreeIn(struct Node *root) 
 {
-  if (root != NULL) {
+  if (root) {
     walkTreeIn(root->left);
     printNode(root);
     walkTreeIn(root->right);
@@ -61,17 +59,16 @@ void walkTreeLevel(struct Node *root)
   struct Queue *queue;
   queue = newQueue(MAXQUEUESIZE);
 
-  if(root != NULL) enqueue(queue, root);
+  if(root) enqueue(queue, root);
 
   while (! isQueueEmpty(queue)) {
     struct Node *next = dequeue(queue);
 
-    if ( next != NULL) {
-      // printf("%4d %4d %4d %4d ", next->Altura,  next->val, next->SomaChaveD,  next->SomaChaveD2);
+    if (next) {
       printNode(next);
 
-      if (next->left != NULL)  enqueue(queue, next->left);
-      if (next->right != NULL) enqueue(queue, next->right);
+      if (next->left)  enqueue(queue, next->left);
+      if (next->right) enqueue(queue, next->right);
     }
   }
   destroyQueue(queue);
@@ -82,17 +79,16 @@ void walkTreeLevelR(struct Node *root)
   struct Queue *queue;
   queue = newQueue(MAXQUEUESIZE);
 
-  if(root != NULL) enqueue(queue, root);
+  if(root) enqueue(queue, root);
 
   while (! isQueueEmpty(queue)) {
     struct Node *next = dequeue(queue);
 
-    if ( next != NULL) {
-      // printf("%4d %4d %4d %4d ", next->Altura,  next->val, next->SomaChaveD,  next->SomaChaveD2);
+    if (next) {
       printNode(next);
 
-      if (next->right != NULL) enqueue(queue, next->right);
-      if (next->left != NULL)  enqueue(queue, next->left);
+      if (next->right) enqueue(queue, next->right);
+      if (next->left)  enqueue(queue, next->left);
     }
   }
   destroyQueue(queue);
@@ -109,10 +105,10 @@ void walkTreeLevelLL(struct Node *root)
   while (! isQueueEmptyLL(queue)) {
     struct Node *next = dequeueLL(queue);
 
-    if ( next != NULL) {
+    if (next) {
       printNode(next);
-      if (next->left != NULL)  enqueueLL(queue, next->left);
-      if (next->right != NULL) enqueueLL(queue, next->right);
+      if (next->left)  enqueueLL(queue, next->left);
+      if (next->right) enqueueLL(queue, next->right);
     }
   }
   destroyQueueLL(queue);
@@ -122,9 +118,10 @@ void walkTreeLevelLL(struct Node *root)
 
 int sizeTree(struct Node *root) 
 {
-  if (root == NULL)
+  if (root)
+    return sizeTree(root->left) + 1 + sizeTree(root->right);
+  else
     return 0;
-  return sizeTree(root->left) + 1 + sizeTree(root->right);
 }
 
 
@@ -134,7 +131,7 @@ int sizeTree(struct Node *root)
  */
 void insertNode(struct Tree *T, struct Node *node) 
 {
-  if (T->root == NULL) {
+  if (!T->root) {
     /* If the tree is empty, work almost done! Just assign it */
     T->root = node;
   } else {
@@ -150,7 +147,7 @@ void insertNode(struct Tree *T, struct Node *node)
     while((t = dequeue(queue))) {
 
       /* If the left side is empty, we found the spot */
-      if (t->left == NULL) {
+      if (!t->left) {
         t->left = node;
         t->left->parent = t;
         break;
@@ -160,7 +157,7 @@ void insertNode(struct Tree *T, struct Node *node)
       }
 
       /* If the right side is empty, we do not care about the queue above */
-      if (t->right == NULL) {
+      if (!t->right) {
         t->right = node;
         t->right->parent = t;
         break;
@@ -181,7 +178,7 @@ void insertNode(struct Tree *T, struct Node *node)
  */
 void insertNodeLL(struct Tree *T, struct Node *node) 
 {
-  if (T->root == NULL) {
+  if (!T->root) {
     /* If the tree is empty, work almost done! Just assign it */
     T->root = node;
   } else {
@@ -197,7 +194,7 @@ void insertNodeLL(struct Tree *T, struct Node *node)
     while((t = dequeueLL(queue))) {
 
       /* If the left side is empty, we found the spot */
-      if (t->left == NULL) {
+      if (!t->left) {
         t->left = node;
         t->left->parent = t;
         break;
@@ -207,7 +204,7 @@ void insertNodeLL(struct Tree *T, struct Node *node)
       }
 
       /* If the right side is empty, we do not care about the queue above */
-      if (t->right == NULL) {
+      if (!t->right) {
         t->right = node;
         t->right->parent = t;
         break;
@@ -224,18 +221,15 @@ void insertNodeLL(struct Tree *T, struct Node *node)
 
 void atualizaAltura(struct Node *root)
 {
-  if(root->left != NULL)
-    atualizaAltura(root->left);
-     
-  if(root->right != NULL)
-    atualizaAltura(root->right);
+  if(root->left) atualizaAltura(root->left);
+  if(root->right) atualizaAltura(root->right);
   
-  if(root->left == NULL && root->right == NULL)
+  if(!(root->left || root->right))
     root->Altura = 1;
   else 
-    if(root->left == NULL && root->right != NULL)
+    if(!root->left && root->right)
       root->Altura = root->right->Altura +1;
-    else if(root->left != NULL && root->right == NULL)
+    else if(root->left && !(root->right))
       root->Altura = root->left->Altura + 1;
     else
       root->Altura = maxint(root->left->Altura, root->right->Altura) + 1;
@@ -244,28 +238,27 @@ void atualizaAltura(struct Node *root)
 
 void atualizaNivel(struct Node *root)
 {
-  if(root->parent == NULL)
+  if(!root->parent)
     root->Nivel = 1;
   else
     root->Nivel = root->parent->Nivel + 1;
 
-  if(root->left != NULL)
+  if(root->left)
     atualizaNivel(root->left);
 
-  if(root->right != NULL)
-    atualizaNivel(root->right);
+  if(root->right) atualizaNivel(root->right);
 
 }
 
 void atualizaSomaChaveD(struct Node *root)
 {
-  if (root != NULL) {
+  if (root) {
     atualizaSomaChaveD(root->left);
     atualizaSomaChaveD(root->right);
 
     root->SomaChaveD = root->val;
-    if (root->left != NULL) root->SomaChaveD += root->left->SomaChaveD;
-    if (root->right != NULL) root->SomaChaveD += root->right->SomaChaveD;
+    if (root->left) root->SomaChaveD += root->left->SomaChaveD;
+    if (root->right) root->SomaChaveD += root->right->SomaChaveD;
   }
 }
 
@@ -273,21 +266,21 @@ void atualizaSomaChaveD(struct Node *root)
 
 void atualizaSomaChaveD2(struct Node *root)
 {
-  if (root != NULL) {
+  if (root) {
     atualizaSomaChaveD2(root->left);
     atualizaSomaChaveD2(root->right);
 
     root->SomaChaveD2 = 0;
 
-    if (root->left != NULL) {
-      if(root->left->left == NULL && root->left->right == NULL) 
+    if (root->left) {
+      if(!(root->left->left || root->left->right)) 
         root->SomaChaveD2 += root->left->val;
       else
         root->SomaChaveD2 += root->left->SomaChaveD2 + root->left->val;
     }
 
-    if (root->right != NULL) {
-      if(root->right->left == NULL && root->right->right == NULL) 
+    if (root->right) {
+      if(!(root->right->left || root->right->right)) 
         root->SomaChaveD2 += root->right->val;
       else
         root->SomaChaveD2 += root->right->SomaChaveD2 + root->right->val;
@@ -300,24 +293,21 @@ void atualizaSomaChaveD2c(struct Node *root)
 {
   root->SomaChaveD2 = root->SomaChaveD - root->val;
 
-  if(root->left != NULL) 
-    atualizaSomaChaveD2(root->left);
-  
-  if(root->right != NULL)
-    atualizaSomaChaveD2(root->right);
+  if(root->left) atualizaSomaChaveD2(root->left);
+  if(root->right) atualizaSomaChaveD2(root->right);
 
 }
 
 
 void atualizaSomaChaveA(struct Node *root)
 {
-  if (root->parent == NULL)
+  if (!root->parent)
     root->SomaChaveA = root->val;
   else
     root->SomaChaveA = root->parent->SomaChaveA + root->val;
 
-  if (root->left != NULL) atualizaSomaChaveA(root->left);
-  if (root->right != NULL) atualizaSomaChaveA(root->right);
+  if (root->left) atualizaSomaChaveA(root->left);
+  if (root->right) atualizaSomaChaveA(root->right);
 
 }
 
@@ -325,8 +315,8 @@ void atualizaSomaChaveA(struct Node *root)
 void atualizaSomaChaveA2(struct Node *root)
 {
   root->SomaChaveA2 = root->SomaChaveA - root->val;
-  if (root->left != NULL) atualizaSomaChaveA2(root->left);
-  if (root->right != NULL) atualizaSomaChaveA2(root->right);
+  if (root->left) atualizaSomaChaveA2(root->left);
+  if (root->right) atualizaSomaChaveA2(root->right);
 }
 
 // void atualizaPNivel(struct Node *root) 
@@ -353,7 +343,7 @@ void atualizaSomaChaveA2(struct Node *root)
 int  compareTree(struct Node *roota, struct Node *rootb)
 {
   int status = 1;
-  if (roota != NULL && rootb !=NULL) {
+  if (roota && rootb) {
     if ( compareTree(roota->left, rootb->left) != compareTree(roota->right, rootb->right)) 
       status = 0;
     if(roota->val != rootb->val) 
@@ -380,7 +370,7 @@ int  compareTree(struct Node *roota, struct Node *rootb)
 
 int eFolha(struct Node *node)
 {
-  return !(node->left && node->right);
+  return !(node->left || node->right);
 }
 
 int somaFolha(struct Node *node)
